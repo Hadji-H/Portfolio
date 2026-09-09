c6c91088ffa639fcb81e5ee86227a3df0e318e84
Hadji <hadji.hamada@hotmail.com>
2026-09-09 11:38:10 +0200

    2ème commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Bonjour, je m'appelle Bertrand")
+const monBouton = document.querySelector("#burger-btn");
+const menu = document.querySelector(".menu");
+
+monBouton.addEventListener("click", function() {
+    menu.classList.toggle("active");
+});
